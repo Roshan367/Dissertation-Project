3b1047d6b8ba876c1faf01214deb955ed5eebb21
Roshan Virdee <roshanvirdee@gmail.com>
2025-12-10 03:42:03 +0000

    Added basic ffnn code

---
+++ model-architecture/feedforward.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import math
+import copy
+
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model, d_ff):
+        super(FeedForward, self).__init__()
+        self.fc1 = nn.Linear(d_model, d_ff)
+        self.fc2 = nn.Linear(d_ff, d_model)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return self.fc2(self.relu(self.fc1(x)))
