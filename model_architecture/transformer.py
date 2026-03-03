e234a7d1902d84732f1503c855154ec452674d4f
Roshan Virdee <roshanvirdee@gmail.com>
2026-03-03 01:22:20 +0000

    Updates to training file for optimisation

--- model_architecture/transformer.py
+++ model_architecture/transformer.py
@@ -44,11 +44,16 @@ class Transformer(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def generate_mask(self, src, tgt):
+        device = src.device
+
         src_mask = (src != 0).unsqueeze(1).unsqueeze(2)
         tgt_mask = (tgt != 0).unsqueeze(1).unsqueeze(3)
         seq_length = tgt.size(1)
         nopeak_mask = (
-            1 - torch.triu(torch.ones(1, seq_length, seq_length), diagonal=1)
+            1
+            - torch.triu(
+                torch.ones(1, seq_length, seq_length, device=device), diagonal=1
+            )
         ).bool()
         tgt_mask = tgt_mask & nopeak_mask
         return src_mask, tgt_mask
